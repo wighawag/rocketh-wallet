d41eb3c7ffbbef07feee57d18fcd3241cb351c77
Wighawag <wighawag@gmail.com>
2018-10-30 10:05:24 +0000

    enable

--- provider/src/HtmlProvider.js
+++ provider/src/HtmlProvider.js
@@ -20,6 +20,8 @@ function HtmlProvider(trustedHost, readURL, opts) {
 	this.counter = 0;
 	this.callbacks = {};
 	this.trustedHost = trustedHost;
+	this.iframeURL = (trustedHost == "*" ? "" : trustedHost) + "/iframe.html"; // TODO do not allow wildcard
+	
 	this.queue = []
 	 
 	var iframeId = "__htmlprovider__";
@@ -27,6 +29,7 @@ function HtmlProvider(trustedHost, readURL, opts) {
 	
 	if(!currentIframe){	
 		currentIframe = document.createElement('iframe');
+		// currentIframe.sandbox = "allow-same-origin allow-scripts"; // TODO ? block from setting domain
 		document.body.appendChild(currentIframe);
 	}else{
 		window.removeEventListener("message", currentIframe.htmlProvider); 
@@ -40,9 +43,11 @@ function HtmlProvider(trustedHost, readURL, opts) {
 	this.pendingUserConfirmation = null;
 
 	this.handleEvent = function(event){
-		if(event.source != self.iframe.contentWindow){
+		if(event.source != self.iframe.contentWindow){ // do not accept message from anything except the iframe
 			return;
 		}
+
+		// need to follow these requirements :
 		if(event.type != "message" || (event.origin != self.trustedHost && self.trustedHost != "*")){ // TODO do not allow wildcard
 			return;
 		}
@@ -50,30 +55,22 @@ function HtmlProvider(trustedHost, readURL, opts) {
 		console.log('receiving message from iframe: ', event.data);
 		
 		var data = event.data;
-		if(data == "ready") {
-			this.iframe.style.display = "none";
-		} else if(data == 'unlock_required') {
-			this.iframe.style.display = "block";
-		} else if(data == 'new_account_required') {
+		if (!data.error && data.requireUserConfirmation) { // no result givem (data.result : old payload)
+			// show popup / cancel old pending
+			if (this.pendingUserConfirmation) {
+				self.executeCallback(this.pendingUserConfirmation.id, 'overriden by new user \'s confirmation request', null);		
+			}
+			this.pendingUserConfirmation = {
+				id: data.id,
+				payload: data.result
+			};
 			this.iframe.style.display = "block";
 		} else {
-			if (!data.error && data.requireUserConfirmation) { // no result givem (data.result : old payload)
-				// show popup / cancel old pending
-				if (this.pendingUserConfirmation) {
-					self.executeCallback(this.pendingUserConfirmation.id, 'overriden by new user \'s confirmation request', null);		
-				}
-				this.pendingUserConfirmation = {
-					id: data.id,
-					payload: data.result
-				};
-				this.iframe.style.display = "block";
-			} else {
-				if(this.pendingUserConfirmation && this.pendingUserConfirmation.id == data.id) {
-					this.iframe.style.display = "none";
-					this.pendingUserConfirmation = null;
-				}
-				self.executeCallback(data.id, data.error, data.result);
+			if(this.pendingUserConfirmation && this.pendingUserConfirmation.id == data.id) {
+				this.iframe.style.display = "none";
+				this.pendingUserConfirmation = null;
 			}
+			self.executeCallback(data.id, data.error, data.result);
 		}
 	}	
 	
@@ -91,7 +88,7 @@ function HtmlProvider(trustedHost, readURL, opts) {
 		}
 		self.queue.length = 0;
 	}
-	this.iframe.src = (this.trustedHost == "*" ? "" : this.trustedHost) + "/iframe.html"; // TODO do not allow wildcard
+	this.iframe.src = this.iframeURL;
 	console.log('loading iframe with ' + this.iframe.src);
 
 	this.setupEngine();
@@ -103,6 +100,19 @@ HtmlProvider.prototype.executeCallback = function(id, error, result) {
 	callback(error, result);
 }
 
+HtmlProvider.prototype.enable = function() {
+	var self = this;
+	return new Promise(function(resolve, reject){
+		console.log('ENABLING');
+		self.sendToIFrame({id:self.counter++, jsonrpc:"2.0", method:'enable', params:[]}, function(error, result){
+			if(error) {
+				reject();
+			} else {
+				resolve(result);
+			}
+		});
+	});
+}
 
 HtmlProvider.prototype.sendToIFrame = function (payload, callback) {
 	console.log('sendToIFrame', payload);
@@ -114,7 +124,6 @@ HtmlProvider.prototype.sendToIFrame = function (payload, callback) {
 	try {
 		this.callbacks[payload.id] = callback;
 		this.iframe.contentWindow.postMessage(payload, this.trustedHost);
-		this.counter++;
 	} catch(e) {
 		callback({message:'ERROR: Couldn\'t postMessage to iframe at '+ this.iframe.location.href,type:"error"});
 	}
@@ -123,7 +132,7 @@ HtmlProvider.prototype.sendToIFrame = function (payload, callback) {
 HtmlProvider.prototype.setupEngine = function() {
 
 	var self = this;
-	var counter = 1;
+	self.counter = 1;
 
 	// static results
 	this.addProvider(new FixtureSubprovider({
@@ -149,7 +158,7 @@ HtmlProvider.prototype.setupEngine = function() {
 	// id mgmt
 	this.addProvider(new HookedWalletSubprovider({
 		getAccounts: function(cb){
-			self.sendToIFrame({id:counter++, jsonrpc:"2.0", method:'eth_accounts', params:[]}, function(error, result) {
+			self.sendToIFrame({id:self.counter++, jsonrpc:"2.0", method:'eth_accounts', params:[]}, function(error, result) {
 				console.log('result from iframe', error, result);
 				cb(error, result);
 			});
@@ -160,13 +169,13 @@ HtmlProvider.prototype.setupEngine = function() {
 			if(txData.data && !(txData.data[0] == '0' && txData.data[1] == 'x')) {
 				txData.data = '0x' + txData.data;	
 			}
-			self.sendToIFrame({id:counter++, jsonrpc:"2.0", method:'eth_signTransaction', params:[txData]}, function(error, result) {
+			self.sendToIFrame({id:self.counter++, jsonrpc:"2.0", method:'eth_signTransaction', params:[txData]}, function(error, result) {
 				console.log('eth_signTransaction from iframe', error, result);
 				cb(error, result);
 			});
 		},
 		signMessage: function(msgParams, cb) {
-			self.sendToIFrame({id:counter++, jsonrpc:"2.0", method:'eth_signMessage', params:[msgParams.from, msgParams.data]}, function(error, result) {
+			self.sendToIFrame({id:self.counter++, jsonrpc:"2.0", method:'eth_signMessage', params:[msgParams.from, msgParams.data]}, function(error, result) {
 				console.log('eth_signMessage from iframe', error, result);
 				cb(error, result);
 			});	
